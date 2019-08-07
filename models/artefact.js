d49d29cc052770c770785854b8c55cea4a66f18c
James Taranto <tarantoj@student.unimelb.edu.au>
2019-08-08 00:00:36 +1000

    add basic artefact schema

---
+++ models/artefact.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const artefactSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    image: String
+});
+
+mongoose.model('artefact', artefactSchema);
