a64c0eb3b7146c712d60a3deed652aae2505a332
James Taranto <tarantoj@student.unimelb.edu.au>
2019-08-14 10:39:37 +1000

    Fix linting

--- models/db.js
+++ models/db.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_PASS = process.env.MONGO_PASS;
+const { MONGO_HOST, MONGO_USER, MONGO_PASS } = process.env;
 
 const dbURI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}`;
 
 const options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    dbName: 'it_project'
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  dbName: 'it_project',
 };
 
 mongoose.connect(dbURI, options).then(
-    () => {
-      console.log('Database connection established!');
-    },
-    err => {
-      console.log('Error connecting Database instance due to: ', err);
-    }
-  );
+  () => {
+    // eslint-disable-next-line no-console
+    console.log('Database connection established!');
+  },
+  (err) => {
+    // eslint-disable-next-line no-console
+    console.log('Error connecting Database instance due to: ', err);
+  },
+);
 
 // require(models);
 
 module.exports = {
-    databaseUrl: dbURI,
+  databaseUrl: dbURI,
 };
