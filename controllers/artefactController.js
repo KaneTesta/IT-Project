82860e0d03ad8928c49d3e23834342e4410698e9
James Taranto <tarantoj@student.unimelb.edu.au>
2019-08-29 11:47:48 +1000

    Modify controllers Use middleware instead of callbacks Remove support for old database scheme Add support for new database scheme

---
+++ controllers/artefactController.js
@@ -0,0 +1,94 @@
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const images = require('../lib/images');
+const oauth2 = require('../lib/oauth2');
+const Artefact = require('../models/artefact');
+
+function checkOwner(req, res, next) {
+	Artefact.findById(req.params.id, (err, artefact) => {
+		if (err) next(err);
+		if (req.user.id === artefact.owner) {
+			next();
+		} else {
+			// Not sure how to handle this, forward the error with next or just redirect?
+			const error = new Error('You do not have permission to modify this artefact');
+			error.statusCode = 403;
+			res.redirect('/user/dashboard');
+		}
+	});
+}
+
+exports.createArtefact = [
+	// Must be logged in
+	oauth2.required,
+
+	// Upload image
+	images.multer.single('image'),
+	images.sendUploadToGCS,
+
+	// Validate Body
+	body('name', 'Name must not be empty.').isLength({ min: 1 }).trim(),
+	body('description', 'Description must not be empty.').isLength({ min: 1 }).trim(),
+
+	// Sanitise Body
+	sanitizeBody('*').escape(),
+
+	// Continue
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		const artefact = new Artefact({
+			name: req.body.name,
+			description: req.body.description,
+			tags: req.body.tags,
+			image: req.file.cloudStorageObject,
+			owner: req.user.id,
+		});
+
+		if (!errors.isEmpty()) {
+			next(errors);
+		} else {
+			artefact.save((err) => {
+				if (err) next(err);
+				res.redirect('/user/dashboard');
+			});
+		}
+	},
+];
+
+exports.editArtefact = [
+	oauth2.required,
+	checkOwner,
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		// TODO Check if image is modified, trigger a delete if so
+		const artefact = new Artefact({
+			name: req.body.name,
+			description: req.body.description,
+			tags: req.body.tags,
+			owner: req.user.id,
+			_id: req.params.id,
+		});
+
+		if (!errors.isEmpty()) {
+			next(errors);
+		} else {
+			Artefact.findByIdAndUpdate(req.params.id, artefact, (err) => {
+				if (err) next(err);
+				res.redirect('/user/dashboard');
+			});
+		}
+	},
+];
+
+exports.deleteArtefact = [
+	oauth2.required,
+	checkOwner,
+	(req, res, next) => {
+		Artefact.findByIdAndDelete(req.params.id, (err) => {
+			if (err) next(err);
+			res.redirect('/user/dashboard');
+		});
+	},
+];
