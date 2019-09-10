57b4573990587cfe455e86bd81082127b563d92f
Joel Launder <jalaunder@gmail.com>
2019-09-10 15:36:14 +1000

    Added more tests

--- test/testUser.js
+++ test/testUser.js
@@ -25,25 +25,15 @@ after((done) => {
 describe('User', function describeUser() {
 	this.timeout(10000);
 
-	let user = null;
 	beforeEach((done) => {
 		// Clear database
-		User.deleteMany({}, () => {
-			// Create a new user
-			const newUser = new User({
-				user_id: 'user1',
-				display_name: 'Test User 1',
-			});
-
-			newUser.save((err, savedUser) => {
-				user = savedUser;
-				done(err);
-			});
+		User.deleteMany({}, (err) => {
+			done(err);
 		});
 	});
 
 	describe('GET /user/search/', () => {
-		it('search user with no query', (done) => {
+		it('search no users with no query', (done) => {
 			chai.request(app)
 				.get('/user/search/')
 				.end((err, res) => {
@@ -52,14 +42,82 @@ describe('User', function describeUser() {
 				});
 		});
 
-		it('search user with matching query', (done) => {
-			const query = user.display_name;
+		it('search no users with query', (done) => {
+			const query = 'query';
 			chai.request(app)
 				.get(`/user/search/${query}`)
 				.end((err, res) => {
 					expect(res, 'Should have status code 200').to.have.status(200);
-					done();
+					expect(res.body, 'Body should have one result').to.be.empty;
+					done(err);
 				});
+		});
+
+		it('search user with no query', (done) => {
+			// Create a new user
+			new User({
+				user_id: 'user1',
+				display_name: 'Test User 1',
+			}).save((err, user) => {
+				if (err) {
+					done(err);
+				} else {
+					const query = '';
+					chai.request(app)
+						.get(`/user/search/${query}`)
+						.end((errReq, res) => {
+							expect(res, 'Should have status code 400').to.have.status(400);
+							done(errReq);
+						});
+				}
+			});
+		});
+
+		it('search user with matching query', (done) => {
+			// Create a new user
+			new User({
+				user_id: 'user1',
+				display_name: 'Test User 1',
+			}).save((err, user) => {
+				if (err) {
+					done(err);
+				} else {
+					const query = user.display_name;
+					chai.request(app)
+						.get(`/user/search/${query}`)
+						.end((errReq, res) => {
+							expect(res, 'Should have status code 200').to.have.status(200);
+							expect(res.body, 'Body should have one result').to.have.length(1);
+							res.body.forEach((result) => {
+								expect(result, 'Result should be an object').to.be.an('object');
+								expect(result.display_name, 'Result name should match query').to.equal(query);
+							});
+
+							done(errReq);
+						});
+				}
+			});
+		});
+
+		it('search user with non-matching query', (done) => {
+			// Create a new user
+			new User({
+				user_id: 'user1',
+				display_name: 'Test User 1',
+			}).save((err, user) => {
+				if (err) {
+					done(err);
+				} else {
+					const query = 'query';
+					chai.request(app)
+						.get(`/user/search/${query}`)
+						.end((errReq, res) => {
+							expect(res, 'Should have status code 200').to.have.status(200);
+							expect(res.body, 'Body should have one result').to.be.empty;
+							done(errReq);
+						});
+				}
+			});
 		});
 	});
 });
