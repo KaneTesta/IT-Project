44b8cfb11dc9df9f399f3d75a83cd0f202be9f28
James Taranto <tarantoj@student.unimelb.edu.au>
2019-09-17 15:33:19 +1000

    Remove redundant getArtefact method

--- models/user.js
+++ models/user.js
@@ -34,31 +34,6 @@ userSchema.statics.getArtefacts = function getArtefacts(userId, done) {
 	});
 };
 
-// Gets visible artefact data visible for a given viewer
-userSchema.statics.getArtefact = function getArtefact(userId, artefactId, done) {
-	userSchema.statics.getArtefacts(userId, (err, artefacts) => {
-		if (err) {
-			done(err, null);
-		} else {
-			// Define function for matching artefacts with the given id
-			const artefactMatch = (artefact) => artefact._id.toString() === artefactId.toString();
-			// Check if the user owns or can view an artefact with the given id
-			const ownerArtefact = artefacts.owner.find(artefactMatch);
-			const viewerArtefact = artefacts.viewer.find(artefactMatch);
-			// Return the artefact
-			if (ownerArtefact) {
-				ownerArtefact.isOwner = true;
-				done(err, ownerArtefact);
-			} else if (viewerArtefact) {
-				viewerArtefact.isOwner = false;
-				done(err, viewerArtefact);
-			} else {
-				done(err, null);
-			}
-		}
-	});
-};
-
 // Create index on name and email for searching
 userSchema.index({
 	display_name: 'text',
